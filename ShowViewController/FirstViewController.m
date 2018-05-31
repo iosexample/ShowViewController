e7affade0b529ca46e249abf42986c9080c7abe9
Dong <donly@magicalboy.com>
2018-05-31 15:05:10 +0800

    first version

--- ShowViewController/FirstViewController.m
+++ ShowViewController/FirstViewController.m
@@ -8,6 +8,10 @@
 #import "FirstViewController.h"
 
 @interface FirstViewController ()
+{
+    BOOL isMenuShow;
+}
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *bottomCons;
 
 @end
 
@@ -16,6 +20,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    isMenuShow = NO;
 }
 
 
@@ -24,5 +29,19 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)showMenuAction:(id)sender {
+//    self.bottomCons.constant = 222;
+//    [self.view layoutIfNeeded];
+    
+    __weak __typeof(self) weakSelf = self;
+    [UIView animateWithDuration:0.5
+                     animations:^{
+                         __strong __typeof(weakSelf)strongSelf = weakSelf;
+                         strongSelf.bottomCons.constant = strongSelf->isMenuShow ? 222 : 0;
+                         [strongSelf.view layoutIfNeeded];
+                         strongSelf->isMenuShow = !strongSelf->isMenuShow;
+                     }];
+
+}
 
 @end
---
+++ ShowViewController/MenuCollectionViewController.h
@@ -0,0 +1,12 @@
+//
+//  MenuCollectionViewController.h
+//  ShowViewController
+//
+//  Created by dong on 31/5/2018.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MenuCollectionViewController : UICollectionViewController
+
+@end
